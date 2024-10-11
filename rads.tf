66b421f701b0b782cabe0826a38db7acf86b4d2c
eatoh <lyneabuangho22@gmail.com>
2024-10-10 20:05:10 -0400

    my first commit

---
+++ rads.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "name" {
+  instance_type = t2.micro
+  iam_instance_profile 
+
+tag =dev
---
+++ s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "name" {
+  bucket name "developer"
+}
+
+resource "aws_instance" "name" {
+    instance_type = 
+    tags_all = 
+  
+}
